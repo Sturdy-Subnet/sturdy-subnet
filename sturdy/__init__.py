c9003916b9cbc63cc1c2ac7e9b58f5b0f884d737
Shr1ftyy <syeamtechdemon@gmail.com>
2025-08-19 09:39:26 +0000

    chore: bump version to 5.0.1

--- sturdy/__init__.py
+++ sturdy/__init__.py
@@ -17,7 +17,7 @@
 # DEALINGS IN THE SOFTWARE.
 
 # Define the version of the template module.
-__version__ = "5.0.0"
+__version__ = "5.0.1"
 version_split = __version__.split(".")
 __spec_version__ = (1000 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
 
